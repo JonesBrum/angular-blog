a2848fd9bdb55c281036773a1fa1602dcee84002
Jones Brum <jovbrum@gmail.com>
2022-11-05 00:39:55 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Como Desenvolver em Angular",
+        "description": "Como desenvolver em Angular de forma eficiente e com muita produtividade.",
+        "photoCover": "https://blog.geekhunter.com.br/wp-content/uploads/2019/10/Um-overview-sobre-o-framework-Angular.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Saiu a nova versão do Angular",
+        "description": "It is a long established fact that a reader will be distracted by. ",
+        "photoCover": "https://images.indepth.dev/images/2022/01/Complete-Guide_-Angular-lifecycle-hooks.jpg"
+    }
+]
